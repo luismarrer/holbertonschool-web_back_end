f1d3ba81ecd45fd75dde1b51daf9ff0896d82b1a
Luismarrer <lmarrero1070@gmail.com>
2024-07-25 19:16:17 -0400

    Add task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,11 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
+
   return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then((results) => {
-      return results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
