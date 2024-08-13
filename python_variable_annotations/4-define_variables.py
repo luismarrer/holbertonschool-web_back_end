a0be08c84f4e92a9f19e6b7c5edecefcdb082bc5
Luismarrer <lmarrero1070@gmail.com>
2024-08-13 16:25:23 -0400

    Add task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Module: 4-define_variables
+
+This module defines a few simple variables with different data types.
+"""
+# Integer variable
+a = 1
+"""int: An integer variable with the value 1."""
+
+# Floating-point variable
+pi = 3.14
+"""float: A floating-point variable representing the value of pi."""
+
+# Boolean variable
+i_understand_annotations = True
+"""bool: A boolean variable indicating understanding of annotations."""
+
+# String variable
+school = "Holberton"
+"""str: A string variable representing the name of the school."""
